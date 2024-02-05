9872fbfa2bdac70578fcc20b5945992533fc1659
saqibtanveer786 <adward797@gmail.com>
2024-02-05 14:42:13 +0500

    chore: remove unnecessary import

--- src/app/lesson/[slug]/page.tsx
+++ src/app/lesson/[slug]/page.tsx
@@ -1,13 +1,11 @@
-import React from "react";
 import dynamic from "next/dynamic";
-import LessonNotAvailable from "@/components/LessonNotAvailable";
 const CharsContainer = dynamic(
   () => import("@/components/TypingArea/CharsContainer"),
   {
     ssr: false,
   }
 );
-import { genRandWordsFromStr } from "@/helper";
+
 import { lessonsList } from "@/data/lessonsList";
 
 export default function page({ params }: { params: { slug: string } }) {
