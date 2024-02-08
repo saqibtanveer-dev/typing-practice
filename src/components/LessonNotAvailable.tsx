535cffcd419f7a3087a0579ccd2f58d3cdbee38b
saqibtanveer786 <adward797@gmail.com>
2024-02-08 11:57:31 +0500

    feat: added a back to home btn on lessonNotAva.. component

--- src/components/LessonNotAvailable.tsx
+++ src/components/LessonNotAvailable.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function LessonNotAvailable() {
   return (
-    <div className="h-full w-full grid place-items-center text-2xl dark:text-gray-500">
-      Lesson Not Available
+    <div className="h-[50%] w-full flex items-center justify-center flex-col gap-4 text-2xl dark:text-gray-500">
+      <h2>Lesson Not Available</h2>
+      <Link
+        href="/"
+        className="text-sm bg-black px-8 py-4 rounded-lg text-white"
+      >
+        Back To Home
+      </Link>
     </div>
   );
 }
