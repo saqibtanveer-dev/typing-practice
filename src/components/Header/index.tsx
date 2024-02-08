1021d03596ac8eda5299891be84be85f3c4898af
saqibtanveer786 <adward797@gmail.com>
2024-02-08 19:12:52 +0500

    fix: removed logo. was looking so ugly

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,7 @@ import React from "react";
 export default function index() {
   return (
     <header className="max-w-7xl mx-auto bg-bg-secondary h-[100px] flex items-center px-8 py-4">
-      <div className="logo">
-        <Image alt="Logo" src="/logo.jpeg" width={165} height={80} />
-      </div>
+      <div className="logo"></div>
       <nav className="nav">
         <ul className="navlist">
           <li className="navitem"></li>
